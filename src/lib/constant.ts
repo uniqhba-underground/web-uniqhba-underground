c571ea30778c7e2ac16c50b141248fcb4e3bbe65
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-05-23 21:42:17 +0800

    [refactor] use cloudflare for deployment

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
 export const SiteName: string = 'UNIQHBA Underground';
 export const SiteDesc: string = 'Menjadi komunitas IT yang mendorong inovasi, pengembangan teknologi, dan mencetak talenta digital unggul untuk kemajuan bangsa';
-export const SiteBaseUrl: string = 'https://web-uniqhba-underground.vercel.app/';
-export const SiteImageUrl: string = 'https://web-uniqhba-underground.vercel.app/img/opengraph.png';
+export const SiteBaseUrl: string = 'https://uniqhbaunderground.pages.dev';
+export const SiteImageUrl: string = 'https://uniqhbaunderground.pages.dev/img/opengraph.png';
 export const SiteAuthor: string = 'Ahmad Rizky Nusantara Habibi';
