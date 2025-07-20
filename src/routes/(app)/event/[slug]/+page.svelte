279986522d2a7bc119c78b1242553c146aed9450
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-07-20 20:12:07 +0800

    [feat] buttons for share event

--- src/routes/(app)/event/[slug]/+page.svelte
+++ src/routes/(app)/event/[slug]/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import { OurEventCategoryMap, type OurEvent, type ParticipantCertificate } from "@/data/events/type";
 	import type { PageData } from "./$types";
-	import { onMount } from "svelte";
 	import CertificateGenerator from "@/partials/events/certificateGenerator.svelte";
 	import { Icon } from "svelte-icons-pack";
-	import { RiBusinessCalendarLine, RiSystemTimeLine, RiUserFacesGroup2Line } from "svelte-icons-pack/ri";
+	import {
+    RiBusinessCalendarLine, RiSystemTimeLine, RiUserFacesGroup2Line,
+    RiDesignEditBoxLine, RiLogosFacebookFill, RiLogosTwitterXFill,
+    RiLogosLinkedinBoxFill, RiLogosWhatsappLine,
+	RiBusinessLinksLine,
+  } from "svelte-icons-pack/ri";
 	import { formatDateID } from "@/lib/formatter";
 	import { PathEvents, PathHome, PathName } from "@/state/page";
-	import { TrOutlineChevronRight } from "svelte-icons-pack/tr";
+	import { TrOutlineBrandTelegram, TrOutlineChevronRight } from "svelte-icons-pack/tr";
 	import Head from "@/partials/Head.svelte";
 	import { LuTicket } from "svelte-icons-pack/lu";
+	import { SiteBaseUrl } from "@/lib/constant";
+	import toast, { Toaster } from "svelte-french-toast";
 
   export let data: PageData | any;
 
@@ -24,12 +30,19 @@
     inputDate.setHours(0, 0, 0, 0);
     return inputDate > today;
   }
+
+  function copyLink() {
+    navigator.clipboard.writeText(SiteBaseUrl + '/events/' + ourEvent.slug);
+    toast.success('Link berhasil disalin');
+  }
 </script>
+
+<Toaster />
 
 <Head
   title="{OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.name}"
   description={ourEvent.description}
-  imageUrl={ourEvent.image_url}
+  imageUrl="{SiteBaseUrl}{ourEvent.image_url}"
 />
 
 {#if isExist}
@@ -37,13 +50,13 @@
     <div class="w-full flex flex-col gap-5 mt-2 md:mt-8 px-5 md:px-0">
       <div class="flex flex-row flex-wrap md:text-base text-sm gap-1 items-center not-prose text-uniqhbaunderground font-semibold">
         <a
-          on:click={() => PathName.set(PathHome)}
+          onclick={() => PathName.set(PathHome)}
           href={PathHome}
           class="hover:underline rounded-full"
         >Beranda</a>
         <Icon src={TrOutlineChevronRight} size={15} className="stroke-neutral-800 stroke-2" />
         <a
-          on:click={() => PathName.set(PathEvents)}
+          onclick={() => PathName.set(PathEvents)}
           href={PathEvents}
           class="hover:underline rounded-full"
         >Events</a>
@@ -70,6 +83,72 @@
               >{OurEventCategoryMap[ourEvent.category]}</span>
               <span class="text-sm border py-1 px-3 rounded-full w-fit other"
               >{ourEvent.field}</span>
+            </div>
+            <div class="flex flex-row flex-wrap gap-2 items-center">
+              <a
+                title="Share to Facebook"
+                target="_blank"
+                href="https://www.facebook.com/sharer.php?u={SiteBaseUrl}/events/{OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.field} {ourEvent.name}"
+                class="bg-uniqhbaunderground hover:bg-uniqhbaunderground/80 text-white p-1.5 rounded-full cursor-pointer"
+              >
+                <Icon
+                  src={RiLogosFacebookFill}
+                  size="16"
+                />
+              </a>
+              <a
+                title="Share to X (Twitter)"
+                target="_blank"
+                href="https://twitter.com/share?url={SiteBaseUrl}/events/{ourEvent.slug}&text={OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.field} {ourEvent.name}"
+                class="bg-uniqhbaunderground hover:bg-uniqhbaunderground/80 text-white p-1.5 rounded-full cursor-pointer"
+              >
+                <Icon
+                  src={RiLogosTwitterXFill}
+                  size="16"
+                />
+              </a>
+              <a
+                title="Share to LinkedIn"
+                target="_blank"
+                href="https://www.linkedin.com/shareArticle?url={SiteBaseUrl}/events/{ourEvent.slug}&title={OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.field} {ourEvent.name}"
+                class="bg-uniqhbaunderground hover:bg-uniqhbaunderground/80 text-white p-1.5 rounded-full cursor-pointer"
+              >
+                <Icon
+                  src={RiLogosLinkedinBoxFill}
+                  size="16"
+                />
+              </a>
+              <a
+                title="Share to WhatsApp"
+                target="_blank"
+                href="https://wa.me/?text={OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.field} {ourEvent.name} {SiteBaseUrl}/events/{ourEvent.slug}"
+                class="bg-uniqhbaunderground hover:bg-uniqhbaunderground/80 text-white p-1.5 rounded-full cursor-pointer"
+              >
+                <Icon
+                  src={RiLogosWhatsappLine}
+                  size="16"
+                />
+              </a>
+              <a
+                title="Share to Telegram"
+                target="_blank"
+                href="https://t.me/share/url?url={SiteBaseUrl}/events/{ourEvent.slug}&title={OurEventCategoryMap[ourEvent.category]} {ourEvent.field}: {ourEvent.field} {ourEvent.name}"
+                class="bg-uniqhbaunderground hover:bg-uniqhbaunderground/80 text-white p-1.5 rounded-full cursor-pointer"
+              >
+                <Icon
+                  src={TrOutlineBrandTelegram}
+                  size="16"
+                />
+              </a>
+              <button
+                title="Copy link"
+                onclick={copyLink}
+                class="bg-transparent hover:bg-neutral-100 border border-neutral-200 p-1.5 rounded-full cursor-pointer">
+                <Icon
+                  src={RiBusinessLinksLine}
+                  size="16"
+                />
+              </button>
             </div>
             <div class="flex flex-col gap-2">
               <h1 class="group-hover:underline font-bold text-2xl md:text-4xl">{ourEvent.name}</h1>
@@ -132,10 +211,14 @@
             <a
               href="{ourEvent.registration_link}"
               target="_blank"
-              class="flex flex-row justify-center items-center w-full md:w-fit py-2 px-auto md:px-4
+              class="flex flex-row gap-2 justify-center items-center w-full md:w-fit py-2 px-auto md:px-4
               bg-uniqhbaunderground hover:bg-uniqhbaunderground/90 text-white font-semibold rounded-md cursor-pointer"
             >
-              Daftar Sekarang
+              <Icon
+                src={RiDesignEditBoxLine}
+                size="16"
+              />
+              <span>Daftar Sekarang</span>
             </a>
           {:else}
             <CertificateGenerator
