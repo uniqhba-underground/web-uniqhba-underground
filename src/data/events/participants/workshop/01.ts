8d8b28acc11b6edc3ee0e0731cfeefd6ab2678a6
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-07-26 09:16:04 +0800

    [feat] add more participant in event ui/ux 02

--- src/data/events/participants/workshop/01.ts
+++ src/data/events/participants/workshop/01.ts
@@ -1,8 +1,26 @@
-import type { ParticipantCertificate } from "@/data/events/type";
+import type { ParticipantCertificate } from '@/data/events/type';
 
 export const ParticipantsMeetUp01UIUX: ParticipantCertificate[] = [
-  {
-    "name": "Ahmad Rizky Nusantara Habibi",
-    "certificateId": "2025X0726W0101"
-  }
-]
+	{ name: 'Erik Maulana', certificateId: '2025X0726W0101' },
+	{ name: 'Ferdinand Issak Pascal ', certificateId: '2025X0726W0102' },
+	{ name: 'Sulistiani', certificateId: '2025X0726W0103' },
+	{ name: 'Silvia meiftahul janna', certificateId: '2025X0726W0104' },
+	{ name: 'Muharromi Ali Ilham', certificateId: '2025X0726W0105' },
+	{ name: 'Muhammad Hamzani Wahid', certificateId: '2025X0726W0106' },
+	{ name: 'MUHAMMAD RIZAL ', certificateId: '2025X0726W0107' },
+	{ name: 'Misbahul Munzir', certificateId: '2025X0726W0108' },
+	{ name: 'I Made Bayu Chaesariada Wiryawan ', certificateId: '2025X0726W0109' },
+	{ name: 'Ida Setiawati ', certificateId: '2025X0726W0110' },
+	{ name: 'NURHAYATI ', certificateId: '2025X0726W0111' },
+	{ name: 'arin 🤪', certificateId: '2025X0726W0112' },
+	{ name: 'Muhammad ulul albab', certificateId: '2025X0726W0113' },
+	{ name: 'RAKYATOL HASANAH ', certificateId: '2025X0726W0114' },
+	{ name: 'Adrian Maulana', certificateId: '2025X0726W0115' },
+	{ name: 'Baiq Selviana Pertiwi ', certificateId: '2025X0726W0116' },
+	{ name: 'Istianah', certificateId: '2025X0726W0117' },
+	{ name: 'Indi sepriani ', certificateId: '2025X0726W0118' },
+	{ name: 'MUHAMMAD TAUFIQ HIDAYAT', certificateId: '2025X0726W0119' },
+	{ name: 'Nuraqilla Waidha Bintang Grendis', certificateId: '2025X0726W0120' },
+	{ name: 'Muhamad Multazam', certificateId: '2025X0726W0121' },
+	{ name: 'Ahmad Rizky Nusantara Habibi', certificateId: '2025X0726W0122' }
+];
